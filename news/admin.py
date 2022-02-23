73e1aa2f7c5d1731a455d3a4e9efe7b929f0b2ac
email <kibetdavidro@gmail.com>
2022-02-23 21:02:13 +0300

    deployment to heroku

---
+++ news/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Article, Editor, tags
+
+
+# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+
+admin.site.register(Editor)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(tags)
